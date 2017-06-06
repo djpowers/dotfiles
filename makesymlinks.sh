3a93add4329fe17a2ddaadc6d47bacc4fdb48a89
Dave Powers <djpowers89@gmail.com>
2017-06-05 22:50:30 -0400

    Create vimrc and add to symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile bashrc gitconfig"    # list of files/folders to symlink in homedir
+files="bash_profile bashrc gitconfig vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
