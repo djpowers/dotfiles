42f835cad575457822557b9df2065c1e0461c570
Dave Powers <djpowers89@gmail.com>
2017-08-09 00:42:00 -0400

    Add global gitignore file and specify in config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile bashrc gitconfig vimrc agignore ctags"    # list of files/folders to symlink in homedir
+files="bash_profile bashrc gitconfig vimrc agignore ctags gitignore_global"    # list of files/folders to symlink in homedir
 
 ##########
 
