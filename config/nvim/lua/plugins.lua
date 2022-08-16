27c411f488d95365ca6f10a19f40e537dc528bce
Dave Powers <djpowers89@gmail.com>
2022-08-16 17:02:53 -0400

    Add neovim plugin config
    - comment out copied over vimrc
    - update symlink script to create directories as needed
    - install packer automatically: https://jdhao.github.io/2021/07/11/from_vim_plug_to_packer/#now-enter-packernvim

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,23 @@
+local fn = vim.fn
+
+vim.g.package_home = fn.stdpath("data") .. "/site/pack/packer/"
+local packer_install_dir = vim.g.package_home .. "/opt/packer.nvim"
+
+local plug_url_format = ""
+if vim.g.is_linux then
+  plug_url_format = "https://hub.fastgit.org/%s"
+else
+  plug_url_format = "https://github.com/%s"
+end
+
+local packer_repo = string.format(plug_url_format, "wbthomason/packer.nvim")
+local install_cmd = string.format("10split |term git clone --depth=1 %s %s", packer_repo, packer_install_dir)
+
+-- Auto-install packer in case it hasn't been installed.
+if fn.glob(packer_install_dir) == "" then
+  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
+  vim.cmd(install_cmd)
+end
+
+-- Load packer.nvim
+vim.cmd("packadd packer.nvim")
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,6 +11,8 @@ olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
 files="
+  config/nvim/init.lua
+  config/nvim/lua/plugins.lua
   config/starship.toml
   rbenv/default-gems
   nvm/default-packages
@@ -36,6 +38,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
+    mkdir -p ~/.$file
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
