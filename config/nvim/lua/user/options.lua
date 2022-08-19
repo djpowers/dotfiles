2cf57cd1d550fd2086288dec064ece1995a16894
Dave Powers <djpowers89@gmail.com>
2022-08-18 22:53:07 -0400

    Convert options to use lua loop

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -1,10 +1,15 @@
--- :help options
-vim.opt.number = true          -- show current line number
-vim.opt.relativenumber = true  -- show relative line numbers
-vim.opt.list = true            -- show unwanted spaces
-vim.opt.smartindent = true     -- automatically insert one extra level of indentation in some cases
-vim.opt.expandtab = true       -- tab key (in insert mode) inserts spaces instead of tab characters
-vim.opt.tabstop = 2            -- width of hard tabstop measured in 'spaces' (maximum width of an actual tab character
-vim.opt.shiftwidth = 2         -- size of an 'indent' measured in spaces
-vim.opt.splitbelow = true      -- force all horizontal splits to go below current window
-vim.opt.splitright = true      -- force all vertical splits to go to the right of current window
+local options = {
+  number = true,          -- show current line number
+  relativenumber = true,  -- show relative line numbers
+  list = true,            -- show unwanted spaces
+  smartindent = true,     -- automatically insert one extra level of indentation in some cases
+  expandtab = true,       -- tab key (in insert mode) inserts spaces instead of tab characters
+  tabstop = 2,            -- width of hard tabstop measured in 'spaces' (maximum width of an actual tab character
+  shiftwidth = 2,         -- size of an 'indent' measured in spaces
+  splitbelow = true,      -- force all horizontal splits to go below current window
+  splitright = true,     -- force all vertical splits to go to the right of current window
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
