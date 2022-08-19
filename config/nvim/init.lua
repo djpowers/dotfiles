700462687d709c5f5f4b30978aa3219c76fa8bf7
Dave Powers <djpowers89@gmail.com>
2022-08-18 20:49:42 -0400

    Add basic nvim-lspconfig mappings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -33,6 +33,14 @@ require("luasnip/loaders/from_vscode").lazy_load()
 require("mason").setup()
 require('mason-lspconfig').setup()
 
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
 -- Completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
