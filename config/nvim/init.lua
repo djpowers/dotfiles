fc665dce7a84d486f41eab61d0dc583a4b4f1f4e
Dave Powers <djpowers89@gmail.com>
2022-08-18 21:54:00 -0400

    Migrate some options and remove unused

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,6 +4,15 @@ require 'plugins'
 -- Set leader to <Space>
 vim.g.mapleader = " "
 
+-- Options configuration
+vim.opt.number = true          -- show current line number
+vim.opt.relativenumber = true  -- show relative line numbers
+vim.opt.list = true            -- show unwanted spaces
+vim.opt.smartindent = true     -- automatically insert one extra level of indentation in some cases
+vim.opt.expandtab = true       -- tab key (in insert mode) inserts spaces instead of tab characters
+vim.opt.tabstop = 2            -- width of hard tabstop measured in 'spaces' (maximum width of an actual tab character
+vim.opt.shiftwidth = 2         -- size of an 'indent' measured in spaces
+
 -- Set colorscheme
 vim.cmd[[colorscheme nord]]
 
@@ -120,34 +129,7 @@ require('leap').set_default_keymaps()
 --" " Initialize plugin system
 --" call plug#end()
 --" 
---" " Use filetype detection, as well as indent and plugins
---" if has('autocmd')
---"   filetype plugin indent on
---" endif
---" " Use syntax highlighting
---" if has('syntax') && !exists('g:syntax_on')
---"   syntax enable
---" endif
---" 
---" set nocompatible              " Explicitly set nocompatible
---" set number                    " Show current line number
---" set relativenumber            " Show relative line numbers
---" set listchars=tab:>-,trail:-  " Set character used to indicate unwanted spaces
---" set list                      " Enable displaying unwanted spaces
---" set smartindent               " Automatically inserts one extra level of indentation in some cases
---" set tabstop=2                 " Width of hard tabstop measured in 'spaces' (maximum width of an actual tab character)
---" set shiftwidth=2              " Size of an 'indent' measured in spaces
---" set expandtab                 " Tab key (in insert mode) inserts spaces instead of tab characters
---" set ruler                     " Show line number, column number, and virtual column number on right side of status bar
---" set laststatus=2              " Always display the status line
---" set backspace=2               " Backspace deletes like most programs in insert mode
---" 
---" set incsearch                 " Start searching before pressing enter
---" set hlsearch                  " Highlight all search pattern matches
---" 
 --" set visualbell                " Use visual flash instead of beep on error
---" set noswapfile                " http://robots.thoughtbot.com/post/18739402579/global-gitignore#comment-458413287
---" set updatetime=250            " Set faster update time (for vim-gitgutter)
 --" 
 --" " Mappings to edit and source vimrc file
 --" nnoremap <leader>ev :vsplit $MYVIMRC<cr>
