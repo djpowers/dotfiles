921cc7c5e77bbb46487d56966272de36f55c97e5
Dave Powers <djpowers89@gmail.com>
2022-08-18 22:31:21 -0400

    Move options to separate file

---
+++ config/nvim/lua/user/options.lua
@@ -0,0 +1,10 @@
+-- :help options
+vim.opt.number = true          -- show current line number
+vim.opt.relativenumber = true  -- show relative line numbers
+vim.opt.list = true            -- show unwanted spaces
+vim.opt.smartindent = true     -- automatically insert one extra level of indentation in some cases
+vim.opt.expandtab = true       -- tab key (in insert mode) inserts spaces instead of tab characters
+vim.opt.tabstop = 2            -- width of hard tabstop measured in 'spaces' (maximum width of an actual tab character
+vim.opt.shiftwidth = 2         -- size of an 'indent' measured in spaces
+vim.opt.splitbelow = true      -- force all horizontal splits to go below current window
+vim.opt.splitright = true      -- force all vertical splits to go to the right of current window
