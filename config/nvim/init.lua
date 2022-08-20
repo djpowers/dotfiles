c1209d2673879861c16f1314d1d3cc4114f3ad98
Dave Powers <djpowers89@gmail.com>
2022-08-19 20:28:37 -0400

    Remove commented out keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -35,10 +35,8 @@ require('mason-lspconfig').setup()
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
--- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
