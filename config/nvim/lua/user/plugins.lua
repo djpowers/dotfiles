437c2a92664f7f4601133a5dac52e629e9fb172a
Dave Powers <djpowers89@gmail.com>
2022-08-21 12:06:16 -0400

    Update automatic packer install code
    - follow example from packer docs: https://github.com/wbthomason/packer.nvim#bootstrapping
    - also similar to approach from Neovim from scratch

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -1,8 +1,10 @@
 local fn = vim.fn
+
+-- Automatically install packer
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-   vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
 end
 
 return require('packer').startup(function(use)
