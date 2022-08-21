a52b48c291a55ac5b8acfbedcdbc0f0f9d5e2b12
Dave Powers <djpowers89@gmail.com>
2022-08-21 15:47:39 -0400

    Protect lsp config calls

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -1,6 +1,16 @@
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+	return
+end
+
+local status_ok, masonLspconfig = pcall(require, "mason-lspconfig")
+if not status_ok then
+	return
+end
+
 -- Set up mason (lsp)
-require("mason").setup()
-require('mason-lspconfig').setup()
+mason.setup()
+masonLspconfig.setup()
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -10,9 +20,15 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 -- Completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-require("mason-lspconfig").setup_handlers {
+local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_ok then
+  return
+end
+
+capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+
+masonLspconfig.setup_handlers {
   function (server_name)
     require("lspconfig")[server_name].setup {
       capabilities = capabilities,
