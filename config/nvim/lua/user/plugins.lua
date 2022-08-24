1c995e00974170105895a827d1c9b85e13d61b38
Dave Powers <djpowers89@gmail.com>
2022-08-24 19:45:31 -0400

    Use capitalized variable name on global variable
    - to appease lua language server

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -3,7 +3,7 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
 end
 
@@ -91,7 +91,7 @@ return require('packer').startup(function(use)
 
    -- Automatically set up your configuration after cloning packer.nvim
    -- Put this at the end after all plugins
-   if packer_bootstrap then
+   if Packer_bootstrap then
      require('packer').sync()
    end
 end)
