9b9bb53cf502031173b1bb26c18fd8325f2f4008
Dave Powers <djpowers89@gmail.com>
2022-08-24 13:24:59 -0400

    Implement auto-format on save with null-ls
    - https://github.com/jose-elias-alvarez/null-ls.nvim#how-do-i-format-files-on-save
    - https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save

--- config/nvim/lua/user/null-ls.lua
+++ config/nvim/lua/user/null-ls.lua
@@ -8,10 +8,26 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   debug = false,
   sources = {
     formatting.prettier,
     diagnostics.eslint_d,
   },
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.formatting({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 }
