ebe75c2e976a6f427a878692698c7e3315b7c9c0
Dave Powers <djpowers89@gmail.com>
2022-08-24 15:58:00 -0400

    Delete old plugin system and ALE config options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -12,32 +12,3 @@ require "user.gitsigns"
 require "user.null-ls"
 require "user.autocommands"
 require "user.plugin-setups"
---" " Specify a directory for plugins
---" call plug#begin('~/.vim/plugged')
---" " Make sure you use single quotes
---" Plug 'dense-analysis/ale'
---" " Initialize plugin system
---" call plug#end()
---" 
---" " Set ALE fixers
---" let g:ale_fixers = {
---"       \   'ruby': ['rubocop'],
---"       \   'javascript': ['prettier', 'eslint'],
---"       \   'vue': ['prettier', 'eslint', 'stylelint'],
---"       \   'html': ['prettier'],
---"       \   'css': ['prettier'],
---"       \   'markdown': ['prettier'],
---"       \   'json': ['prettier'],
---"       \}
---" 
---" " Override the invoked Rubocop binary (to run bundled version)
---" let g:ale_ruby_rubocop_executable = 'bundle'
---" 
---" " Have ALE fix files automatically on save
---" let g:ale_fix_on_save = 1
---" 
---" " Show ALE suggestions during typing
---" let g:ale_completion_enabled = 1
---" 
---" " Use ALE's function for omnicompletion
---" set omnifunc=ale#completion#OmniFunc
