3699ac61a22c5d69593f952606b776091ecaade4
Dave Powers <djpowers89@gmail.com>
2022-08-25 17:56:06 -0400

    Fix telescope file browser keymap conflict
    - had copied examples from both telescope and file browser extension,
      which both use "fb" shortcut
    - first result on github for "telescope file_browser" used "fe" example
    - works as "file explorer," also similar to netrw "explore" command

--- config/nvim/lua/user/telescope.lua
+++ config/nvim/lua/user/telescope.lua
@@ -17,7 +17,7 @@ telescope.load_extension "file_browser"
 
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>fb",
+  "<leader>fe",
   [[<cmd>lua require "telescope".extensions.file_browser.file_browser()<CR>]],
   { noremap = true }
 )
