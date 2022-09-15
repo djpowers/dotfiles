3c27622c8ad69e925cb5de8371da03570c9b9b0d
Dave Powers <djpowers89@gmail.com>
2022-09-14 22:07:48 -0400

    Overwrite with lualine default configuration

--- config/nvim/lua/user/lualine.lua
+++ config/nvim/lua/user/lualine.lua
@@ -37,23 +37,63 @@ local spaces = function()
   return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
+-- lualine.setup {
+--   options = {
+--     globalstatus = true,
+--     icons_enabled = true,
+--     theme = "auto",
+--     component_separators = { left = "", right = "" },
+--     section_separators = { left = "", right = "" },
+--     disabled_filetypes = { "alpha", "dashboard" },
+--     always_divide_middle = true,
+--   },
+--   sections = {
+--     lualine_a = { "mode" },
+--     lualine_b = {"branch"},
+--     lualine_c = { diagnostics },
+--     lualine_x = { diff, spaces, "encoding", filetype },
+--     lualine_y = { location },
+--     lualine_z = { "progress" },
+--   },
+-- }
+
 lualine.setup {
   options = {
-    globalstatus = true,
     icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = { "alpha", "dashboard" },
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
     always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
   },
   sections = {
-    lualine_a = { "mode" },
-    lualine_b = {"branch"},
-    lualine_c = { diagnostics },
-    lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_y = { location },
-    lualine_z = { "progress" },
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
   },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
-
