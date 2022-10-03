9c395b4ee580ab977e3d8f4b21f770fc04febd06
Dave Powers <djpowers89@gmail.com>
2022-10-03 11:14:08 -0400

    Update null-ls deprecated method
    - vim.lsp.buf.formatting_sync is deprecated. Use vim.lsp.buf.format instead

--- config/nvim/lua/user/null-ls.lua
+++ config/nvim/lua/user/null-ls.lua
@@ -26,7 +26,7 @@ null_ls.setup {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.formatting_sync()
+          vim.lsp.buf.format()
         end,
       })
     end
