87d19d12a992e8e6709b91a59967b9e21f46fe00
Dave Powers <djpowers89@gmail.com>
2023-02-05 21:32:57 -0500

    Add neotest mappings for running and viewing tests
    - mappings inspired by various configs found via search
    - example: https://github.com/jbsilva/dotfiles/blob/6c5c3c6008761d916671072516be674f78f2b5b2/.config/nvim/lua/plugins/config/neotest.lua
    - example: https://github.com/lukerops/nvim_config/blob/21cb85ba9266af82a01f193296cc91d247beddf3/lua/config/neotest.lua
    - example: https://github.com/JLineaweaver/dotfiles/blob/8d37a30d834721483b1789ab3d66303b5164535c/neovim/.config/nvim/lua/config/keybinds.lua

--- config/nvim/lua/user/neotest.lua
+++ config/nvim/lua/user/neotest.lua
@@ -15,6 +15,17 @@
     }
   })
 
+-- Run the nearest test
+vim.api.nvim_set_keymap('n', 'tn', [[<cmd>lua require("neotest").run.run()<CR>]], { noremap = true })
+-- Run the current file
+vim.api.nvim_set_keymap('n', 'tf', [[<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>]], { noremap = true })
+-- Re-run the last position
+vim.api.nvim_set_keymap('n', 'tl', [[<cmd>lua require("neotest").run.run_last()<CR>]], { noremap = true })
+-- Open the output of a test result
+vim.api.nvim_set_keymap('n', 'to', [[<cmd>lua require("neotest").output.open()<CR>]], { noremap = true })
+-- Toggle the summary window
+vim.api.nvim_set_keymap('n', 'ts', [[<cmd>lua require("neotest").summary.toggle()<CR>]], { noremap = true })
+
 -- Jump to previous/next test failure (adapted from :h neotest.jump)
 vim.api.nvim_set_keymap('n', '[n', [[<cmd>lua require("neotest").jump.prev({ status = "failed" })<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', ']n', [[<cmd>lua require("neotest").jump.next({ status = "failed" })<CR>]], { noremap = true })
