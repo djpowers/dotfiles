cbc3346dde5766f5d032996bfa2922e11ed9abb5
Dave Powers <djpowers89@gmail.com>
2023-02-24 22:18:07 -0500

    Add keyboard shortcut to close current pane
    - using same shortcut as default close tab action
    - closes panes first, then tabs
    - https://wezfurlong.org/wezterm/config/lua/keyassignment/CloseCurrentPane.html

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,4 +6,12 @@ return {
   font_size = 13,
   line_height = 1.2,
   window_decorations = "RESIZE",
+  keys = {
+    -- Closes the current pane. If that was the last pane in the tab, closes the tab.
+    {
+      key = 'w',
+      mods = 'CMD',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+  },
 }
