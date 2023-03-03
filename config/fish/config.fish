352c3a6c602577fbcc639ae4ed6b03d180005d46
Dave Powers <djpowers89@gmail.com>
2023-03-03 08:59:31 -0500

    Restructure fish config file
    - wrap entirety of config in is-interactive guard
    - update path to use homebrew first so packages are available
    - move starship init to end per documentation

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,12 @@
-set -gx EDITOR nvim
-starship init fish | source
-
-fish_add_path /opt/homebrew/bin
-
 if status is-interactive
+  # ensure homebrew is in path
+  fish_add_path /opt/homebrew/bin
+
+  # set variables
+  set -gx EDITOR nvim
+
   # use the default theme
   fish_config theme choose "fish default"
+
+  starship init fish | source
 end
